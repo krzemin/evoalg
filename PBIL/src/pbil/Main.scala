9a419ad770092e5f697959e62e6484083417012c
Piotr Krzemiński <pio.krzeminski@gmail.com>
2013-10-16 08:46:48 +0200

    PBIL added (wip)

---
+++ PBIL/src/pbil/Main.scala
@@ -0,0 +1,51 @@
+package pbil
+
+object PBIL {
+  
+  val rand = scala.util.Random
+  val d = 128
+  
+  type ProbabilityVector = List[Double]
+  type Individual = List[Int]
+  type Population = List[Individual]
+  
+  def binaryRandom(p: Double): Int =
+    if (rand.nextDouble < p) 1 else 0
+  
+  def initProbabilityVector: ProbabilityVector =
+    List.fill(d)(0.5)
+  
+  def randomIndividual(p: ProbabilityVector): Individual =
+    p.map(binaryRandom)
+  
+  def randomPopulation(p: ProbabilityVector, N: Int): Population =
+    List.fill(N)(randomIndividual(p))
+  
+  def orderIndividuals(F: Function[Individual, Int])(x1: Individual, x2: Individual) =
+    if(F(x1) > F(x2)) (x1, x2) else (x2, x1)
+  
+  var n = 500
+  def terminationCondition: Boolean = {
+    n = n - 1
+    n == 0
+  }
+  
+  def printVector(x: Individual): String = x match {
+    case Nil => ""
+    case (x :: xs) => x.toString ++ printVector(xs)
+  }
+  
+  def PBIL(F: Function[Individual, Int])(N: Int, th1: Double, th2: Double, th3: Double): Individual = {
+    
+  }
+  
+}
+
+
+object Main {
+
+  def main(args: Array[String]): Unit = {
+    println("Hello, world!")
+  }
+
+}
