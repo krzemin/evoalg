1100f4d85e94f444ed268a8b7e5ef4f1e410eaa9
Piotr Krzeminski <pio.krzeminski@gmail.com>
2013-10-19 13:20:51 +0200

    running CGA and PBIL sequentially

--- GeneticAlgorithms/src/Main.scala
+++ GeneticAlgorithms/src/Main.scala
@@ -2,11 +2,20 @@
 import genetic.Genetic.printIndividual
 import genetic.Functions._
 import genetic.CompactGeneticAlgorithm.CGA
+import genetic.PopulationBasedIncrementalLearning.PBIL
 
 
 object Main extends App {
 
-  CGA(oneMax, 200, 2000, 0.01).foreach { case ((individual, evaluation)) =>
+  println("=============== CGA: ")
+
+  CGA(oneMax, 128, 1000, 0.01).foreach { case ((individual, evaluation)) =>
+    println(evaluation.toString ++ ": " ++ printIndividual(individual))
+  }
+
+  println("=============== PBIL: ")
+
+  PBIL(oneMax, 100, 128, 1000, 0.1, 0.5, 0.01).foreach { case ((individual, evaluation)) =>
     println(evaluation.toString ++ ": " ++ printIndividual(individual))
   }
 
