830da5b818187f1bcedb4d8e3fc8084ac3cefdbe
Piotr Krzemiński <pio.krzeminski@gmail.com>
2013-10-20 12:50:05 +0200

    Bugfixed merging results from multiple threads

--- GeneticAlgorithms/src/main/scala/Main.scala
+++ GeneticAlgorithms/src/main/scala/Main.scala
@@ -20,9 +20,7 @@ object Main {
   }
 
   def showResult(result: List[(Individual, Int)]) = {
-    var i = 0
-    result.foreach { case ((individual, evaluation)) =>
-      i += 1
+    (1 to result.size).zip(result).foreach { case (i, (individual, evaluation)) =>
       println(i.toString ++ ": " ++ evaluation.toString ++ "|" ++ printIndividual(individual))
     }
   }
@@ -41,39 +39,26 @@ object Main {
                                  th2: Double,
                                  th3: Double): List[(Individual, Int)] = {
     val threadIndividualSize = individualSize / noThreads
-
     val pool: ExecutorService = Executors.newFixedThreadPool(noThreads)
-
     def createFuture() = new FutureTask[List[(Individual, Int)]](new Callable[List[(Individual, Int)]]() {
       def call(): List[(Individual, Int)] = {
         PBIL(oneMax, populationSize, threadIndividualSize, steps, th1, th2, th3, new scala.util.Random)
       }})
-
     val futures  = List.fill(noThreads)(createFuture())
-
     futures.foreach { (f) =>  pool.execute(f) }
-
-
     val partials = time( futures.map( _.get() ) )
-
-    val result = time ( partials.reduceLeft { (l1, l2) =>
-      for(
-        left <- l1;
-        right <- l2
-      ) yield (left._1, left._2 + right._2)
-    }                   )
-
+    val result: List[(Individual, Int)] = time ( partials.reduceLeft { (l1: List[(Individual, Int)], l2:List[(Individual, Int)]) =>
+      (l1, l2).zipped map { case (((ind1,eva1), (ind2,eva2))) => (ind1++ind2, eva1+eva2) }
+    } )
     pool.shutdown()
-
     result
   }
-
 
   def main(args: Array[String]) = {
     println("Running genetic algorithm...")
     //val result = CGA(oneMax, 1024, 15000, 0.004)
     //val result = time( PBIL(oneMax, 512, 1024, 1000, 0.001, 0.3, 0.1) )
-    val result = time( runPBILOneMaxMultiThreaded(2, 512, 1024, 1000, 0.001, 0.3, 0.1) )
+    val result = time( runPBILOneMaxMultiThreaded(4, 128, 1024, 2000, 0.01, 0.003, 0.01) )
 
     println("Algorithm finished. Displaying chart...")
     showChart(result)
